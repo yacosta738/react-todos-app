ed30c5014646c830e2c127a4d2d1e2cd706b7455
Yuniel Acosta Pérez <yuniel.acosta@cav.desoft.cu>
2021-06-14 09:23:06 -0400

    first commit

---
+++ src/components/TodoItem.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+export default class TodoItem extends React.Component {
+    render() {
+        return <li>
+            <input
+                type="checkbox"
+                checked={this.props.todo.completed}
+                onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+            />
+            {this.props.todo.title}
+
+            <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+                Delete
+            </button>
+        </li>
+    }
+}
