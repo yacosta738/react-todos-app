cc2864f830b864f1b4943c5c95d1d0153d190ec1
Yuniel Acosta Pérez <yuniel.acosta@cav.desoft.cu>
2021-06-14 11:09:40 -0400

    full app working.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom"
 
 //component
 import TodoContainer from "./functionBased/components/TodoContainer"
